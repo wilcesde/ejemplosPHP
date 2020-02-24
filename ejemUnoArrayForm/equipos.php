6c98edd687626dd01291b6c0d57d2fa899460f3b
wilcesde <wilcascesde@gmail.com>
2020-02-23 20:48:48 -0500

    First commit

---
+++ ejemUnoArrayForm/equipos.php
@@ -0,0 +1,12 @@
+<?php
+
+$nomEquipos=array($_POST["equi1"],$_POST['equi2']);
+
+echo "<hr>" . $nomEquipos[0],
+"<br>", $nomEquipos[1],"<hr>";
+
+/*print_r($nomEquipos);
+
+var_dump($nomEquipos);
+
+ */
